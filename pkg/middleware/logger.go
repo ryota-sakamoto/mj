8122edb1f61da7b3e2422d052b5645a60f4bc79d
Ryota Sakamoto <sakamo.ryota+github@gmail.com>
2023-04-29 23:54:21 +0900

    use slog.SetDefault

--- pkg/middleware/logger.go
+++ pkg/middleware/logger.go
@@ -12,18 +12,20 @@ func Logger() grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 		start := time.Now()
 
+		slog.SetDefault(slog.Default().With(
+			slog.Any("request_id", ctx.Value(requestIDKey{})),
+		))
+
 		res, err := handler(ctx, req)
 		if err != nil {
 			slog.Error(
 				"handler error",
-				slog.Any("request_id", ctx.Value(requestIDKey{})),
 				slog.Any("error", err),
 			)
 		}
 
 		slog.Info(
 			"access log",
-			slog.Any("request_id", ctx.Value(requestIDKey{})),
 			slog.String("method", info.FullMethod),
 			slog.String("elapsed", time.Since(start).String()),
 		)
