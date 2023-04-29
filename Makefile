a81727d71ded5c5521b427f613c972f1881d4f65
Ryota Sakamoto <sakamo.ryota+github@gmail.com>
2023-04-29 23:32:34 +0900

    add clinet

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ dev_api:
 	go run cmd/api/main.go
 
 dev_client:
-	go run cmd/api/main.go
+	go run cmd/client/main.go
 
 gen:
 	protoc --go_out=. --go-grpc_out=. proto/*
--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"context"
 
+	"golang.org/x/exp/slog"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"github.com/ryota-sakamoto/mj/pkg/pb"
+)
+
+func main() {
+	conn, err := grpc.Dial(
+		":8080",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	client := pb.NewRoomServiceClient(conn)
+	res, err := client.Create(context.TODO(), &pb.CreateRoomRequest{})
+	if err != nil {
+		panic(err)
+	}
+
+	slog.Info("create room res", slog.Any("res", res))
 }
